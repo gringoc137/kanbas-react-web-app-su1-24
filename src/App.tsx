7f599e19ebbf302f34f60b1e40e959a36ae4ea3c
unknown <athuldinesh@gmail.com>
2024-05-10 11:55:48 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Labs from './Labs';
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to webdev</h1>
+      <Labs />
+    </div>
+  );
+}
+
+export default App;
